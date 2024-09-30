767fc9a55bf29799e28bf6eec0c4f7378afd2c0e
GervinFung <gervinfungdaxuen@gmail.com>
2024-09-30 20:43:13 +0800

    lint(*): use `ci-info` for `isLocal`

--- src/api/database/index.ts
+++ src/api/database/index.ts
@@ -1,6 +1,8 @@
 import type { ObjectId } from 'mongoose';
 
+import ci from 'ci-info';
 import mongoose from 'mongoose';
+import { string, object, array, parse, custom } from 'valibot';
 
 import config from './config';
 import { contactFormMessageSchema } from './schema';
@@ -12,20 +14,22 @@ type ContactFormMessage = Readonly<{
 }>;
 
 export default class Database {
-	private readonly client: ReturnType<typeof mongoose.connect>;
-	private readonly collections: typeof config.collections;
-
 	private static readonly create = () => {
 		const environment = process.env.NEXT_PUBLIC_NODE_ENV;
 
 		const isLocal =
-			environment === 'testing' || environment === 'development';
+			ci.isCI ||
+			environment === 'testing' ||
+			environment === 'development';
 
 		const url = isLocal
 			? `mongodb://${config.auth.user}:${config.auth.password}@${config.address}:${config.port}/${config.database}?authSource=admin&retryWrites=true&w=majority`
 			: `mongodb+srv://${config.auth.user}:${config.auth.password}@${config.address}/${config.database}?authSource=admin&retryWrites=true&w=majority`;
 
-		return new this(url, config.collections);
+		return new this({
+			client: mongoose.connect(url),
+			collections: config.collections,
+		});
 	};
 
 	private static database: Database | undefined = undefined;
@@ -40,33 +44,53 @@ export default class Database {
 		return this.database;
 	};
 
-	private constructor(url: string, collections: typeof config.collections) {
-		this.client = mongoose.connect(url);
+	private constructor(
+		private readonly props: Readonly<{
+			client: ReturnType<typeof mongoose.connect>;
+			collections: typeof config.collections;
+		}>
+	) {}
 
-		this.collections = collections;
-	}
+	readonly collections = () => {
+		return this.props.collections;
+	};
+
+	readonly client = () => {
+		return this.props.client;
+	};
 
 	// testing purpose only
 	readonly close = async () => {
-		return (await this.client).disconnect();
+		return (await this.client()).disconnect();
 	};
-
 	readonly clearCollections = async () => {
 		return await this.getContactFormMessage().deleteMany({});
 	};
-
 	readonly getAllContactFormMessages = async () => {
 		try {
 			const contactFormMessage = this.getContactFormMessage();
 
 			return {
 				result: 'succeed',
-				messages: await contactFormMessage
-					.find<ContactFormMessage>(
-						{},
-						{ name: 1, email: 1, message: 1 }
-					)
-					.exec(),
+				messages: parse(
+					array(
+						object({
+							name: string(),
+							email: string(),
+							message: string(),
+						})
+					),
+					await contactFormMessage
+						.find(
+							{},
+							{
+								name: 1,
+								email: 1,
+								message: 1,
+							}
+						)
+						.exec()
+				),
 			} as const;
 		} catch (error) {
 			return {
@@ -78,7 +102,7 @@ export default class Database {
 
 	private readonly getContactFormMessage = () => {
 		return mongoose.model(
-			this.collections.contactFormMessage,
+			this.collections().contactFormMessage,
 			contactFormMessageSchema
 		);
 	};
@@ -96,26 +120,31 @@ export default class Database {
 
 			return {
 				result: 'succeed',
-				message: contactFormMessage
-					.findOne<
-						ContactFormMessage &
-							Readonly<{
-								_id: ObjectId;
-							}>
-					>(
-						{
-							_id: result._id,
-						},
-						{
-							projection: {
-								_id: 1,
-								name: 1,
-								email: 1,
-								message: 1,
+				message: parse(
+					object({
+						_id: custom<ObjectId>((input) =>
+							mongoose.isValidObjectId(input)
+						),
+						name: string(),
+						email: string(),
+						message: string(),
+					}),
+					await contactFormMessage
+						.findOne(
+							{
+								_id: result._id,
 							},
-						}
-					)
-					.exec(),
+							{
+								projection: {
+									_id: 1,
+									name: 1,
+									email: 1,
+									message: 1,
+								},
+							}
+						)
+						.exec()
+				),
 			} as const;
 		} catch (error) {
 			return {
@@ -125,5 +154,3 @@ export default class Database {
 		}
 	};
 }
-
-export type { ContactFormMessage };
